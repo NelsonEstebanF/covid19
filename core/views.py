70bd386e65b4f9e1cf13d5a75e7e6c7650ebe90f
NelsonEstebanF <esteban1922@hotmail.com>
2021-09-26 10:06:52 -0300

    formatdate

--- core/views.py
+++ core/views.py
@@ -19,22 +19,22 @@ countries.sort()
 def covid(request):
     if request.method=='POST':
         country = request.POST['selectedcountry']
-        weekReport=[]
+        pointStart_date = today - timedelta( days= 7)
+        new_cases =[]
+        deaths_per_day =[]
+        weekReport =[]
+        weekReport.append(pointStart_date)
         for i in range(7):
-            day = today - timedelta( days = i)
+            day = today - timedelta( days= i)
             querystring = { "country": country ,"day": day }
             response = requests.request("GET", url_history, headers=headers, params=querystring).json()
             response = response['response']
             if response:
                 response = response[len(response)-1]
-                new_cases = response['cases']['new']
-                deaths_per_day = response['deaths']['new']
-                date = response['day']
-                weekReport.append({
-                    'new_cases' : int(new_cases),
-                    'deaths_per_day' : int(deaths_per_day),
-                    'date' : date
-                })
+                new_cases.append(int(response['cases']['new'])) 
+                deaths_per_day.append(int(response['deaths']['new']))
+        weekReport.append(new_cases)
+        weekReport.append(deaths_per_day)
         pprint.pprint(weekReport)
    
         for i in allStatistics:
